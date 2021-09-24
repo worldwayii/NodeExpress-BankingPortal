69f91ee8c348b3360320ccb93f6e2d6aadf1b4f3
worldwayii <eluichuma@gmail.com>
2021-09-24 16:12:29 +0100

    Fix express path

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.render('index', {title: 'Index'});
