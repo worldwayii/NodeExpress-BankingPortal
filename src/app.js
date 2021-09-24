9b1ac48aec3d50ec11f5bfb6b223c5209743c65b
worldwayii <eluichuma@gmail.com>
2021-09-24 17:16:44 +0100

    Fix module2 tasks 1-10

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ const accounts = accountData;
 app.get('/', function(req, res){
     res.render('index', {title: 'Account Summary', account: accounts });
 });
+app.get('/savings', function(req, res){
+    res.render('account', {title: 'Savings Summary', account: accounts.savings });
+});
+app.get('/credit', function(req, res){
+    res.render('account', {title: 'Credit Summary', account: accounts.credit });
+});
+app.get('/profile', function(req, res){
+    res.render('profile', {title: 'Profile Summary', user: users[0] });
+});
 
 
 const userData =JSON.parse(fs.readFileSync( path= '/src/json/accounts.json',  encoding= 'UTF8' ));
