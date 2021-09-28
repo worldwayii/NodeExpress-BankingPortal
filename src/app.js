8aabbba253793cf17da7be7fe077db1cd8e1804d
worldwayii <eluichuma@gmail.com>
2021-09-28 14:17:19 +0100

    Finished module5

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = new express();
 
 const { accounts, users, writeJSON } = require('./data.js');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
@@ -14,26 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
-app.get('/transfer', (req, res) =>  res.render('transfer'));
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-});
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
