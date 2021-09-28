df00021e89c8166a2803e16c43a627aac4864733
worldwayii <eluichuma@gmail.com>
2021-09-28 12:54:59 +0100

    Finish up module 3

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.post('/payment', (req, res) =>{
     accounts.credit.available += parseInt(req.body.amount);
     accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    res.render({ message: 'Payment Successful', account: accounts.credit });
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
 
