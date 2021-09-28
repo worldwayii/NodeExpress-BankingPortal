995482c93cf135c5879537f03d2753a84a39c48d
worldwayii <eluichuma@gmail.com>
2021-09-28 13:28:26 +0100

    Finish up module 4

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
-fs = require('fs');
-path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
@@ -7,13 +7,13 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-const writeJson = () => {
-    accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+const writeJSON = () => {
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
 }
 
 module.exports = {
-    accounts,
-    users,
-    writeJson
+  accounts,
+  users,
+  writeJSON
 };
